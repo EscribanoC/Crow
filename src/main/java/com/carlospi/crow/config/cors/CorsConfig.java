2af559223095e3b0c16d8d93458825663aa79bac
Carlos <escribano.flores.carlos@iescamas.es>
2025-05-04 21:29:20 +0200

    Docker

--- src/main/java/com/carlospi/crow/config/cors/CorsConfig.java
+++ src/main/java/com/carlospi/crow/config/cors/CorsConfig.java
@@ -15,6 +15,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
