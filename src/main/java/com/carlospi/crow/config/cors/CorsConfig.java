7bf92d3a40c17451e86639c225263bd8334af823
Carlos <escribano.flores.carlos@iescamas.es>
2025-05-08 13:50:34 +0200

    cambios

--- src/main/java/com/carlospi/crow/config/cors/CorsConfig.java
+++ src/main/java/com/carlospi/crow/config/cors/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://20.39.234.26:5173")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
